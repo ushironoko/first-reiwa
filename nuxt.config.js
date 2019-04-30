b509e8a940a9a001e2b083f260df4b2bf439c4a5
ushironoko <apple19940820@yahoo.co.jp>
2019-05-01 00:13:13 +0900

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,13 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Born The Reiwa',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/rei.png' }]
   },
 
   /*
