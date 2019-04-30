740ed39889f07af7b56a733b9f01824d735a232a
ushironoko <apple19940820@yahoo.co.jp>
2019-04-30 23:57:23 +0900

    reiwa

--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,17 @@
     <div>
       <logo />
       <h1 class="title">
-        first-reiwa
+        Reiwa に なったよ
       </h1>
       <h2 class="subtitle">
-        My premium Nuxt.js project
+        Born The Reiwa
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
 <style>
